1abcee6fac0d56b69c800e1d1469f34ea4df9cfb
Starport75 <weber.austint@gmail.com>
2022-09-19 16:28:13 -0500

    Set up the Ping Command

--- amiiBot_v2/src/main/java/amiiBot_v2/Main.java
+++ amiiBot_v2/src/main/java/amiiBot_v2/Main.java
@@ -7,22 +7,13 @@ public class Main {
 	
 	static boolean debugMode = true;
 	
-	public static void main(String[] args) {
-		// Insert your bot's token here
-		
+	public static void main(String[] args) {		
 		FileAccess file = new FileAccess(debugMode);
 		String token = file.getDiscordToken();
-
 		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
-		// Add a listener which answers with "Pong!" if someone writes "!ping"
-		api.addMessageCreateListener(event -> {
-			if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-				event.getChannel().sendMessage("Pong!");
-			}
-		});
-
-		// Print the invite url of your bot
 		System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
-	}
+		
+		new PingCommand(api);
+		}
 }
---
+++ amiiBot_v2/src/main/java/amiiBot_v2/PingCommand.java
@@ -0,0 +1,50 @@
+package amiiBot_v2;
+
+import java.util.List;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.message.MessageFlag;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandUpdater;
+
+public class PingCommand {
+	SlashCommand thisCommand;
+
+	// Spot to set the name and description of the command
+	String commandName = "ping";
+	String commandDescription = "Pings the bot!";
+
+	public PingCommand(DiscordApi api) {
+		List<SlashCommand> globalCommands = api.getGlobalSlashCommands().join();
+
+		int i = 0;
+		boolean isInitalized = false;
+
+		while (i < globalCommands.size() & !isInitalized) {
+			if (globalCommands.get(i).getName().equals(commandName)) {
+				isInitalized = true;
+				thisCommand = globalCommands.get(i);
+			}
+			i++;
+		}
+
+		if (!isInitalized) {
+			thisCommand = SlashCommand.with(commandName, commandDescription).createGlobal(api).join();
+		}
+
+		thisCommand = new SlashCommandUpdater(thisCommand.getId()).setDescription(commandDescription)
+				// Spot to add any unique stuff to the command
+				
+				.updateGlobal(api).join();
+
+		api.addSlashCommandCreateListener(event -> {
+			SlashCommandInteraction slashCommandInteraction = event.getSlashCommandInteraction();
+			if (slashCommandInteraction.getCommandName().equals(commandName)) {
+				//Spot for command response
+				slashCommandInteraction.createImmediateResponder().setContent("Pong!").respond();
+			}
+		});
+
+	}
+}
